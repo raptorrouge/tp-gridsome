4505bb34c2790c6a9086eb6c81a5ec5a80cb2c9d
raptor <julescrecre@gmail.com>
2021-11-10 09:10:53 +0100

    template Projet.vue

--- src/pages/Projets.vue
+++ src/pages/Projets.vue
@@ -1,9 +1,27 @@
 <template>
   <Layout>
     <h1>Liste des projets</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <ul>
+      <li v-for="{node:projet} of $page.allProjet.edges" :key="projet.id">
+        <g-link :to="projet.path">{{projet.title}}</g-link>
+      </li>
+    </ul>
   </Layout>
 </template>
+
+<page-query>
+query {
+  allProjet {
+    edges {
+      node {
+        id
+        path
+        title
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
---
+++ src/templates/Projet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="projet">
+    <h1>{{$page.projet.title}}</h1>
+    <p><img :src="$page.projet.imageURL" width="100" alt=""></p>
+    <p><time>Date : {{ this.$page.projet.date }}</time></p>
+    <ul>
+      <li v-for="tag of $page.projet.tag" :key="tag">{{tag}}</li>
+    </ul>
+    <div class="content" v-html="$page.projet.content"></div>
+  </div>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  projet(id: $id) {
+    title
+    imageURL
+    date(format: "DD MMMM YYYY", locale: "fr")
+    tag
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: "Projet",
+  metaInfo() {
+    return {
+      title: this.$page.projet.title,
+    }
+  }
+}
+</script>
