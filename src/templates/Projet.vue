754f58485bb3523b64a9f03409451a1648db7a12
raptor <julescrecre@gmail.com>
2021-11-17 16:22:34 +0100

    test menu sur projet

--- src/templates/Projet.vue
+++ src/templates/Projet.vue
@@ -1,4 +1,14 @@
 <template>
+  <header class="header">
+    <strong>
+      <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+    </strong>
+    <nav class="nav">
+      <g-link class="nav__link" to="/">Home</g-link>
+      <g-link class="nav__link" to="/projets/">Projets</g-link>
+      <g-link class="nav__link" to="/about/">About</g-link>
+    </nav>
+  </header>
   <div class="projet">
     <h1>{{$page.projet.title}}</h1>
     <p><img :src="$page.projet.imageURL" width="100" alt=""></p>
