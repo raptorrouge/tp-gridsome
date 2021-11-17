937fe681639065b910e4cb7f27028b7c8547bdd8
raptor <julescrecre@gmail.com>
2021-11-17 16:33:56 +0100

    menu ok dans projet

--- src/templates/Projet.vue
+++ src/templates/Projet.vue
@@ -1,23 +1,15 @@
 <template>
-  <header class="header">
-    <strong>
-      <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-    </strong>
-    <nav class="nav">
-      <g-link class="nav__link" to="/">Home</g-link>
-      <g-link class="nav__link" to="/projets/">Projets</g-link>
-      <g-link class="nav__link" to="/about/">About</g-link>
-    </nav>
-  </header>
-  <div class="projet">
-    <h1>{{$page.projet.title}}</h1>
-    <p><img :src="$page.projet.imageURL" width="100" alt=""></p>
-    <p><time>Date : {{ this.$page.projet.date }}</time></p>
-    <ul>
-      <li v-for="tag of $page.projet.tag" :key="tag">{{tag}}</li>
-    </ul>
-    <div class="content" v-html="$page.projet.content"></div>
-  </div>
+  <layout>
+    <div class="projet">
+      <h1>{{$page.projet.title}}</h1>
+      <p><img :src="$page.projet.imageURL" width="100" alt=""></p>
+      <p><time>Date : {{ this.$page.projet.date }}</time></p>
+      <ul>
+        <li v-for="tag of $page.projet.tag" :key="tag">{{tag}}</li>
+      </ul>
+      <div class="content" v-html="$page.projet.content"></div>
+    </div>
+  </layout>
 </template>
 
 <page-query>
